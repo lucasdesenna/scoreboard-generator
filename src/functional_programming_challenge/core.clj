6413c085a3907b306a5ca68f7f48bff4035bb0b0
Lucas de Senna <lucasdesenna@gmail.com>
2016-02-29 14:12:22 -0300

    Input file parser

--- src/functional_programming_challenge/core.clj
+++ src/functional_programming_challenge/core.clj
@@ -1,7 +1,13 @@
 (ns functional-programming-challenge.core
   (:gen-class))
 
+(require 'functional-programming-challenge.fileParser)
+(refer 'functional-programming-challenge.fileParser)
+
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (def test-file "resources/input.txt")
+  (parse-file test-file))
+
+(-main)
