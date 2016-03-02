0583c3600934692a1a748850c1a239fd0d17542f
Lucas de Senna <lucasdesenna@gmail.com>
2016-03-02 16:48:13 -0300

    FileParser

---
+++ src/scoreboard_generator/fileParser.clj
@@ -0,0 +1,27 @@
+(ns scoreboard-generator.fileParser
+  (:gen-class)
+  (:require scoreboard-generator.invitation)
+  (:refer scoreboard-generator.invitation))
+
+(defn- extract-lines-from-file
+  "Extracts all lines from the input file and returns them as a list of single line strings."
+  [file-path]
+  (let [reader clojure.java.io/reader 
+        stream (reader file-path) 
+        lines (with-open [line-sequence stream] (doall (line-seq line-sequence)))]
+    lines))
+
+(defn- parse-invitations-from-lines
+  "Parses all valid lines from the input collection into a list of invitations."
+  [lines]
+  (for [line lines 
+        :let [invite (invitation line)]
+        :when invite] 
+    invite))
+
+(defn parse-invitations-from-file 
+  "Parses the file designated by the path into a list of invitations."
+  [file-path]
+  (let [lines (extract-lines-from-file file-path) 
+        invitations (parse-invitations-from-lines lines)] 
+    invitations))
