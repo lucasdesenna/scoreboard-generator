0bc834c72de49d3acc6890a3d24eb9cd13a0e675
Lucas de Senna <lucasdesenna@gmail.com>
2016-03-02 16:51:59 -0300

    FIX project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject functional-programming-challenge "0.1.0-SNAPSHOT"
+(defproject scoreboard-generator "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot functional-programming-challenge.core
+  :main ^:skip-aot scoreboard-generator.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
